e4e494f8a22e88be828d2ed6a6be1406fe908577
Erno Pakarinen <erno@codesmith.fi>
2014-10-23 20:10:36 +0300

    Tester app uses the new debugging macro

--- tester/src/CTesterGame.cpp
+++ tester/src/CTesterGame.cpp
@@ -1,36 +1,40 @@
 #include "CTesterGame.h"
 #include <iostream>
 #include <unistd.h>
+#include <csmeg/CDebug.h>
 
 using namespace std;
 
 CTesterGame::CTesterGame() : m_Counter(0)
 {
-    cout << "CTesterGame created" << endl;
+    LOG_INFO() << "CTesterGame created.";
 }
 
 CTesterGame::~CTesterGame()
 {
+    LOG_INFO() << "CTesterGame destroyed.";
 }
 
 void CTesterGame::onLoadContent()
 {
+    LOG_INFO() << "Performing LoadContent";
 }
 
 void CTesterGame::onUnloadContent()
 {
+    LOG_INFO() << "Performing UnloadContent";
 }
 
 void CTesterGame::onDraw(const CGameTime& gameTime) const
 {
-    cout << "OnRender: " << m_Counter << endl;
-    cout << "Elapsed: " << gameTime.getTotalMsec() << endl;
+    LOG_INFO() << "OnRender: " << m_Counter;
+    LOG_INFO() << "Elapsed: " << gameTime.getTotalMsec();
 }
 
 void CTesterGame::onUpdate(const CGameTime& gameTime)
 {
     m_Counter++;
-    cout << "OnUpdate: " << m_Counter << endl;
+    LOG_INFO() << "OnUpdate: " << m_Counter;
 }
 
 void CTesterGame::onEvent(SDL_Event& event)
@@ -38,7 +42,7 @@ void CTesterGame::onEvent(SDL_Event& event)
     if(event.type == SDL_KEYDOWN) {
         const SDL_KeyboardEvent& keyEvent = event.key;
         if(keyEvent.state == SDL_PRESSED) {
-            cout << "Keypressed: " << keyEvent.keysym.sym << endl;
+            LOG_INFO() << "Keypressed: " << keyEvent.keysym.sym;
             if(keyEvent.keysym.sym == SDLK_ESCAPE ) {
                 stop();
             }
@@ -48,6 +52,7 @@ void CTesterGame::onEvent(SDL_Event& event)
 
 bool CTesterGame::onInitialize()
 {
+    LOG_INFO() << "Performing Initialize()";
     // Game OnInitialize is called before the graphics context and SDL are initialized
     // Resolution etc can be set here.
     setUpdateInterval(300); // msec
@@ -59,5 +64,5 @@ bool CTesterGame::onInitialize()
 
 void CTesterGame::onRelease()
 {
-    cout << "OnRelease" << endl;
+    LOG_INFO() << "Performing Release()";
 }
--- tester/src/main.cpp
+++ tester/src/main.cpp
@@ -3,17 +3,21 @@
 #include <iostream>
 #include <exception>
 
+#include <csmeg/CDebug.h>
 
 using namespace std;
 using namespace csmeg;
 
 int main()
 {
+    Debug::CLogger::setLogLevel(DebugLogLevel::INFO);
+    LOG_INFO() << "CSMEG Tester application starting up";
     try {
         CTesterGame game;
         game.run();
     } catch( exception& e ) {
-        cerr << "Error: " << e.what() << endl;
+        LOG_ERROR() << "Exception, reason: " << e.what();
     }
+    LOG_INFO() << "All done.";
     return 0;
 }
